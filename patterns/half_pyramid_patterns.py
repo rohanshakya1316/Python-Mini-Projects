9951a8abb76845625602aa1b81076fb933f1bd59
rohanshakya1316 <romanrohanreignsshakya@gmail.com>
2026-05-31 14:18:53 +0545

    Right-Angle Half Pyramid Pattern

--- patterns/half_pyramid_patterns.py
+++ patterns/half_pyramid_patterns.py
@@ -9,3 +9,8 @@ print("\nInverted Left-Angled Half Pyramid")
 for i in range(rows, 0, -1):
     print("* " * i, end="")
     print("\r")
+
+print("\nRight-Angled Half Pyramid")
+for i in range(1, rows + 1):
+   print("  " * (rows - i) + "* " * i, end="")
+   print("\r")
