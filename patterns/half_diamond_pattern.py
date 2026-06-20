3b35521a685502fd20764995896e9c9cdf1f5af7
rohanshakya1316 <romanrohanreignsshakya@gmail.com>
2026-06-20 14:28:13 +0545

    Left-Angled Half Diamond Pattern

---
+++ patterns/half_diamond_pattern.py
@@ -0,0 +1,10 @@
+rows = int(input("Enter the rows: "))
+
+print("\nLeft-Angled Half Diamond")
+for i in range(1, rows + 1):
+    print("* " * i, end="")
+    print("\r")
+
+for i in range(rows - 1, 0, -1):
+    print("* " * i, end="")
+    print("\r")
