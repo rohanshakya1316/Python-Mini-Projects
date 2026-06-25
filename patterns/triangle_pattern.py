1ba2639bf7214f49658ca3fc2da33fb36868b914
rohanshakya1316 <romanrohanreignsshakya@gmail.com>
2026-06-25 12:03:05 +0545

    Triangle Pattern

---
+++ patterns/triangle_pattern.py
@@ -0,0 +1,9 @@
+def trianglePattern(n):
+    for i in range(0, n):
+        for j in range(0, i+1):
+            print("* ", end="")
+        print("\r")
+
+num = int(input("Enter number of rows: "))
+
+trianglePattern(num)
