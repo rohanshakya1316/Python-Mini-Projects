95e7457c389b1c04abb12996c5ef028d8b9fbecd
rohanshakya1316 <romanrohanreignsshakya@gmail.com>
2026-06-25 14:18:29 +0545

    Revert "Right-Angled Half Diamond Pattern"
    This reverts commit 4f228b690489f997904beaf5edf049ddac6721cf.

--- patterns/half_diamond_pattern.py
+++ patterns/half_diamond_pattern.py
@@ -8,12 +8,3 @@ for i in range(1, rows + 1):
 for i in range(rows - 1, 0, -1):
     print("* " * i, end="")
     print("\r")
-
-print("\nRight-Angled Half Diamond")
-for i in range(1, rows + 1):
-   print("  " * (rows - i) + "* " * i, end="")
-   print("\r")
-
-for i in range(rows - 1, 0, -1):
-   print("  " * (rows - i) + "* " * i, end="")
-   print("\r")
